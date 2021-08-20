0762f58eb205f9e529972a5f3014c445f5c94f17
Darren Rodricks <86697301+Darrenrodricks@users.noreply.github.com>
2021-08-20 12:26:05 -0700

    Add files via upload

---
+++ GitProjects/IntToString.py
@@ -0,0 +1,11 @@
+# Define a function that can convert a integer into a string and print it in console.
+def intChanger(n) -> int:
+    b = str(n)
+    print(b)
+
+intChanger(100)
+intChanger(200)
+intChanger(300)
+intChanger(490)
+intChanger(784)
+intChanger(674)
