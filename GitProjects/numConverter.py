206e8a3a983458515894d0fd7585077ebbff791f
Darren Rodricks <86697301+Darrenrodricks@users.noreply.github.com>
2021-08-20 12:26:21 -0700

    Add files via upload

---
+++ GitProjects/numConverter.py
@@ -0,0 +1,5 @@
+# Define a function that can receive two integral numbers in string form and compute their sum and then print it in console.
+def num_adder(num1, num2):
+    print(int(num1) + int(num2))
+
+num_adder("3","4")
