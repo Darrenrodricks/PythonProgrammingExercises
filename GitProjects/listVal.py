c7cf0f023ed2b2a2da7f818fc07be24c87120946
Darren Rodricks <86697301+Darrenrodricks@users.noreply.github.com>
2021-08-20 14:25:32 -0700

    Add files via upload

---
+++ GitProjects/listVal.py
@@ -0,0 +1,8 @@
+# Define a function which can generate and print a list where the values are square of numbers between 1 and 20 (both included).
+def square_list():
+    l = list()
+    for i in range(1, 21):
+        l.append(i ** 2)
+    print(l)
+
+square_list()
