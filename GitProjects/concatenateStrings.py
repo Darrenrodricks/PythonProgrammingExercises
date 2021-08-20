8c6a8ea4002ac159d68711375bb81ccb6df00b69
Darren Rodricks <86697301+Darrenrodricks@users.noreply.github.com>
2021-08-20 12:26:39 -0700

    Add files via upload

---
+++ GitProjects/concatenateStrings.py
@@ -0,0 +1,5 @@
+# Define a function that can accept two strings as input and concatenate them and then print it in console.
+def string_adder(string1, string2):
+    print(string1 + string2)
+
+string_adder("3","4")
