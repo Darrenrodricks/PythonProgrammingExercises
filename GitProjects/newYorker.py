7bcbd9d65592c153c2b80e8d64d5a3da17c1e5d1
Darren Rodricks <86697301+Darrenrodricks@users.noreply.github.com>
2021-08-22 15:37:08 -0700

    Add files via upload

---
+++ GitProjects/newYorker.py
@@ -0,0 +1,12 @@
+# Define a class named American and its subclass NewYorker.
+class American():
+    pass
+
+class NewYorker(American):
+    pass
+
+a = American
+ny = NewYorker
+
+print(American)
+print(NewYorker)
