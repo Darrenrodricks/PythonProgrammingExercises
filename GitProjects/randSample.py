de8ddec41d71186d7a20a371a0f32c9e8ebefc31
Darren Rodricks <86697301+Darrenrodricks@users.noreply.github.com>
2021-08-23 08:20:37 -0700

    Add files via upload

---
+++ GitProjects/randSample.py
@@ -0,0 +1,3 @@
+# Please write a program to output a random number, which is divisible by 5 and 7, between 0 and 10 inclusive using random module and list comprehension.
+import random
+print(random.sample(range(100, 200), 5))
