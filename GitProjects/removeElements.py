4f0aa4263c01da476a47389096ad228cbd0d01c2
Darren Rodricks <86697301+Darrenrodricks@users.noreply.github.com>
2021-08-23 08:47:03 -0700

    Add files via upload

---
+++ GitProjects/removeElements.py
@@ -0,0 +1,4 @@
+# By using list comprehension, please write a program to print the list after removing the 0th, 2nd, 4th,6th numbers in [12,24,35,70,88,120,155].
+a  = [12,24,35,70,88,120,155]
+a = [x for (i, x) in enumerate(a) if i % 2 != 0]
+print(a)
