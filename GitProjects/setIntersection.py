637a036fded622d5b9e9d27012a1f95cd84730a3
Darren Rodricks <86697301+Darrenrodricks@users.noreply.github.com>
2021-08-23 08:45:50 -0700

    Add files via upload

---
+++ GitProjects/setIntersection.py
@@ -0,0 +1,7 @@
+# With two given lists [1,3,6,78,35,55] and [12,24,35,24,88,120,155], write a program to make a list whose elements
+# are intersection of the above given lists.
+set1= {1, 3, 6, 78, 35, 55}
+set2= {12, 24, 35, 24, 88, 120, 155}
+set1 &= set2
+a=list(set1)
+print(a)
